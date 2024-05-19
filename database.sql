f83c8e7e81cd277daa2a1b0354db1ed67b578275
BWalsh08 <bwalsh@mail.endicott.edu>
2024-05-19 16:55:07 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -33,3 +33,18 @@ CREATE TABLE comments (
 
 mysql -u root -p
 
+<?php
+$servername = "localhost";
+$username = "";
+$password = "BrenWal123";
+$dbname = "forum";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
